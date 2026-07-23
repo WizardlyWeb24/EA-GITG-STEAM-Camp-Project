4d040c01aa09ba98a030173addd21fcd0ff1c546
Raunak Sharma <115401822+WizardlyWeb24@users.noreply.github.com>
2026-07-23 15:23:23 -0400

    Finished title and ending screens

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,15 +3,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject menuScreen;
+    public GameObject ExitScreen;
 
     public void PlayGame()
     {
         menuScreen.SetActive(false);
+        ExitScreen.SetActive(false);
     }
     public void QuitGame()
     {
         Debug.Log("Application closing");
         Application.Quit();
     }
+    public void ShowEndingScreen()
+    {
+        ExitScreen.SetActive(true);
+    }
+    public void ReturnToMenu()
+    {
+        ExitScreen.SetActive(false);
+        menuScreen.SetActive(true);
+    }
+
 }
 
